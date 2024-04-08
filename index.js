2386fbe18bdf4a99501095867f0b05e17529477f
JeanKim7 <jean.g.km@gmail.com>
2024-04-07 17:52:56 -0700

    Add hidden country display card

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 let countrySearchForm = document.getElementById('country-search-form')
-countrySearchForm.addEventListener('submit', event => displayInfo(event))
+countrySearchForm.addEventListener('submit', event => findCountry(event))
 
-function displayInfo(event){
+function findCountry(event){
     event.preventDefault();
     console.log("Searching for info about your country!");
-    let countryInput = document.getElementById('ccountry-input')
-    
+    let countryName = document.getElementById('country-input')?.value;
+    console.log(`Searching for ${countryName}`)
+
+    const url = `https://restcountries.com/v3.1/name/${countryName}`
+    console.log(url)
+
+    fetch(url)
+        .then(res => res.json())
+        .then (data => displayCountry(data))
+        .catch(err => console.error(err))
+}
+
+function displayCountry(data){
+    console.log(data)
+    document.getElementById('hidden').style.display = "flex"
+
+    document.getElementById('flag-img').src=data[0][]
+    console.log(data[0]['name'])
 }
